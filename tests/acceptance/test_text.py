d7483ba00e019c966e645c8fb6a0c0600c3fb33f
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-05-04 10:37:55 +0200

    Testing TEXT example

---
+++ tests/acceptance/test_text.py
@@ -0,0 +1,40 @@
+import re
+import unittest
+import pexpect
+
+
+class ListTest(unittest.TestCase):
+    def setUp(self):
+        self.sut = pexpect.spawn ('python examples/text.py')
+
+    def set_name(self, name='foo'):
+        self.sut.expect("What's your name.*", timeout=1)
+        self.sut.sendline(name)
+
+    def set_surname(self, surname='bar'):
+        self.sut.expect("What's your surname.*", timeout=1)
+        self.sut.sendline(surname)
+
+    def set_phone(self, phone='123456789'):
+        self.sut.expect("What's your phone.*", timeout=1)
+        self.sut.sendline(phone)
+
+    def test_default_input(self):
+        self.set_name()
+        self.set_surname()
+        self.set_phone()
+        self.sut.expect_list([re.compile("'name': 'foo'"),
+                              re.compile("'surname': 'bar'"),
+                              re.compile("'phone': '123456789'")],
+                             timeout=1)
+
+    def test_invalid_phone(self):
+        self.set_name()
+        self.set_surname()
+        self.set_phone('abcde')
+        self.sut.expect('Invalid value.*', timeout=1)
+        self.sut.sendline('12345')
+        self.sut.expect_list([re.compile("'name': 'foo'"),
+                              re.compile("'surname': 'bar'"),
+                              re.compile("'phone': '12345'")],
+                             timeout=1)
