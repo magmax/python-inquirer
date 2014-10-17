26ba987644282e295fd2e83429e022f5003a4895
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-10-17 06:12:08 +0200

    Fixing a problem during installation, where it was requiring the blessings module

--- inquirer/__init__.py
+++ inquirer/__init__.py
@@ -1,6 +1,11 @@
+from __future__ import print_function
+
 __version__ = '1.0.3'
 
-from .prompt import prompt
-from .questions import Text, Password, Confirm, List, Checkbox
+try:
+    from .prompt import prompt
+    from .questions import Text, Password, Confirm, List, Checkbox
 
-__all__ = ['prompt', 'Text', 'Password', 'Confirm', 'List', 'Checkbox']
+    __all__ = ['prompt', 'Text', 'Password', 'Confirm', 'List', 'Checkbox']
+except ImportError as e:
+    print("An error was found, but returning just with the version: %s" % e)
