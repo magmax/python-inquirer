1067d02bea78707c6de2ba8f4ee3d9842f407ae8
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-12-03 19:27:49 +0100

    Making the theme optional for doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,10 @@
 
 import sys
 import os
-import sphinx_bootstrap_theme
+try:
+    import sphinx_bootstrap_theme
+except ImportError:
+    sphinx_bootstrap_theme = None
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -111,7 +114,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
+html_theme = 'bootstrap' if sphinx_bootstrap_theme else 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -120,7 +123,8 @@ html_theme = 'bootstrap'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+if sphinx_bootstrap_theme:
+    html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
