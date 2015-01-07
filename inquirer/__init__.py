cb7e33f9573aeeb3ce0c2d7b736ccdea9e7fa25b
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2015-01-07 19:21:58 +0100

    exporting load_from_json

--- inquirer/__init__.py
+++ inquirer/__init__.py
@@ -8,6 +8,6 @@ try:
         load_from_dict, load_from_json
 
     __all__ = ['prompt', 'Text', 'Password', 'Confirm', 'List', 'Checkbox',
-               'load_from_dict', 'load_from_json']
+               'load_from_list', 'load_from_dict', 'load_from_json']
 except ImportError as e:
     print("An error was found, but returning just with the version: %s" % e)
