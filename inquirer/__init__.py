ad17e70bb38eb90c2df909a75b2fb0a027f077de
Steven Loria <sloria1@gmail.com>
2017-06-17 12:07:25 -0400

    Add shortcut functions
    These make it simpler to present single inputs

--- inquirer/__init__.py
+++ inquirer/__init__.py
@@ -6,8 +6,10 @@ try:
     from .prompt import prompt
     from .questions import Text, Password, Confirm, List, Checkbox, \
         load_from_dict, load_from_json
+    from .shortcuts import text, password, confirm, list_input, checkbox
 
     __all__ = ['prompt', 'Text', 'Password', 'Confirm', 'List', 'Checkbox',
-               'load_from_list', 'load_from_dict', 'load_from_json']
+               'load_from_list', 'load_from_dict', 'load_from_json',
+               'text', 'password', 'confirm', 'list_input', 'checkbox']
 except ImportError as e:
     print("An error was found, but returning just with the version: %s" % e)
