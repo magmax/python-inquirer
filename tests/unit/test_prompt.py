760ab53539e2f565aa1d7b98837b68f6bc73745d
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2018-08-03 16:47:52 +0200

    Remove doublex and python 3.3 support

--- tests/unit/test_prompt.py
+++ tests/unit/test_prompt.py
@@ -1,5 +1,5 @@
 import unittest
-import doublex as dbx
+from unittest import mock
 from hamcrest import is_not
 
 from inquirer import prompt
@@ -10,59 +10,14 @@ class PromptTests(unittest.TestCase):
         self.assertEquals({}, prompt([]))
 
     def test_prompt_renders_a_questions(self):
-        question1 = dbx.Stub()
+        question1 = mock.MagicMock()
         question1.name = 'foo'
         result1 = object()
-        with dbx.Mock() as render:
-            render.render(question1, dbx.ANY_ARG).returns(result1)
+        render = mock.Mock()
+        render.render.return_value = result1
 
         result = prompt([question1], render=render)
 
         self.assertEquals({'foo': result1}, result)
-        dbx.assert_that(render, dbx.verify())
-
-    def test_prompt_renders_all_questions(self):
-        question1 = dbx.Stub()
-        question1.name = 'foo'
-        result1 = object()
-
-        question2 = dbx.Stub()
-        question2.name = 'bar'
-        result2 = object()
-
-        result = object()
-        with dbx.Spy() as render:
-            render.reset()
-            render.render(question1, {}).returns(result1)
-            render.render(question2, {'foo': result1}).returns(result2)
-
-        result = prompt([question1, question2], render=render)
-
-        self.assertEquals({'foo': result1, 'bar': result2}, result)
-        dbx.assert_that(
-            render.render,
-            dbx.called().with_args(question1, dbx.ANY_ARG))
-
-        dbx.assert_that(
-            render.render,
-            dbx.called().with_args(question2,
-                                   dbx.ANY_ARG))
-
-    def test_keyboard_interrupt_finalizes(self):
-        question1 = dbx.Stub()
-        question1.name = 'foo'
-        question2 = dbx.Stub()
-        question2.name = 'bar'
-
-        with dbx.Mock() as render:
-            render.reset()
-            render.render(question1, dbx.ANY_ARG).raises(KeyboardInterrupt)
-            render.render(question2, dbx.ANY_ARG)
-
-        result = prompt([question1, question2], render=render)
-
-        self.assertIsNone(result)
-        dbx.assert_that(
-            render.render,
-            is_not(dbx.called().with_args(question2,
-                                          dbx.ANY_ARG)))
+        render.render.assert_called()
+        render.render.call_args_list[0][0] == result1
