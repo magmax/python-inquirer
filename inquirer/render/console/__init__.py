9c51928bfa29adfbce2e4ff46455bb64b69e5034
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2018-09-16 15:19:06 +0200

    fi flakes

--- inquirer/render/console/__init__.py
+++ inquirer/render/console/__init__.py
@@ -90,7 +90,8 @@ class ConsoleRender(object):
                        "{tq.mark_color}?{tq.brackets_color}]{t.normal} {msg}"
 
         # ensure any user input with { or } will not cause a formatting error
-        escaped_current_value = render.get_current_value().replace('{', '{{').replace('}', '}}')
+        escaped_current_value =
+            render.get_current_value().replace('{', '{{').replace('}', '}}')
         self.print_str(
             '\n%s: %s' % (msg_template, escaped_current_value),
             msg=header,
