efa52f7f0b1c406d993a65f1adac6af487301de5
AuHau <uhlir.a@gmail.com>
2018-09-23 12:18:11 -0700

    Hiding of values in Passport validation messages

--- inquirer/render/console/_password.py
+++ inquirer/render/console/_password.py
@@ -7,3 +7,5 @@ class Password(Text):
     def get_current_value(self):
         return self.question.echo * len(self.current)
 
+    def handle_validation_error(self, error):
+        return 'Entered value is not a valid {q}.'.format(q=self.question.name)
