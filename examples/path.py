d10da17abe01a2c2680ab7556a8bce959bce3a77
AuHau <uhlir.a@gmail.com>
2019-05-24 12:09:35 -0700

    Adding Path example

---
+++ examples/path.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from pprint import pprint
+
+import inquirer
+
+sys.path.append(os.path.realpath('.'))
+
+questions = [
+    inquirer.Path('path',
+                  message="Give me some any type of path"),
+    inquirer.Path('directory', path_type=inquirer.Path.DIRECTORY,
+                  message="Give me directory"),
+    inquirer.Path('file', path_type=inquirer.Path.FILE,
+                  message="Give me file"),
+    inquirer.Path('existing_file', path_type=inquirer.Path.FILE, exists=True,
+                  message="Give me existing file",
+                  )
+]
+
+answers = inquirer.prompt(questions)
+
+pprint(answers)
