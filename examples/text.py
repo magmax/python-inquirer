e458268e2aa6f119e785001f1011ecc485a861c0
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2021-03-01 07:23:31 +0100

    fix tests

--- examples/text.py
+++ examples/text.py
@@ -1,30 +1,27 @@
 import os
-import sys
 import re
+import sys
 from pprint import pprint
 
-import inquirer
-from inquirer import errors
-
-sys.path.append(os.path.realpath('.'))
+sys.path.append(os.path.realpath("."))
+import inquirer  # noqa
 
 
 def phone_validation(answers, current):
-    if not re.match('\+?\d[\d ]+\d', current):
-        raise errors.ValidationError('', reason='I don\'t like your phone number!')
+    if not re.match(r"\+?\d[\d ]+\d", current):
+        raise inquirer.errors.ValidationError("", reason="I don't like your phone number!")
 
     return True
 
 
 questions = [
-    inquirer.Text('name',
-                  message="What's your name?"),
-    inquirer.Text('surname',
-                  message="What's your surname, {name}?"),
-    inquirer.Text('phone',
-                  message="What's your phone number",
-                  validate=phone_validation,
-                  )
+    inquirer.Text("name", message="What's your name?"),
+    inquirer.Text("surname", message="What's your surname, {name}?"),
+    inquirer.Text(
+        "phone",
+        message="What's your phone number",
+        validate=phone_validation,
+    ),
 ]
 
 answers = inquirer.prompt(questions)
