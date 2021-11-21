1a0235ef810678509d07db084a27b255a0d79317
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2021-11-21 02:29:55 +0100

    #94 Document ignore

---
+++ examples/ignore.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from pprint import pprint
+
+sys.path.append(os.path.realpath("."))
+import inquirer  # noqa
+
+questions = [
+    inquirer.Text("name", message="What's your name?"),
+    inquirer.Text(
+        "surname", message="What's your surname, {name}?", ignore=lambda x: x["name"].lower() == "anonymous"
+    ),
+    inquirer.Confirm("married", message="Are you married?"),
+    inquirer.Text("time_married", message="How long have you been married?", ignore=lambda x: not x["married"]),
+]
+
+answers = inquirer.prompt(questions)
+
+pprint(answers)
