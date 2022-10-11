5eb6dfd3ec6e4d4c6b8617f49f7ee3375da0c2e1
William Venner <14863743+WilliamVenner@users.noreply.github.com>
2022-10-11 12:05:38 +0100

    Implement tab cycling for auto completion

--- src/inquirer/render/console/_text.py
+++ src/inquirer/render/console/_text.py
@@ -11,7 +11,7 @@ class Text(BaseConsoleRender):
         super().__init__(*args, **kwargs)
         self.current = self.question.default or ""
         self.cursor_offset = 0
-        self._autocomplete_state = 0
+        self._autocomplete_state = None
 
     def get_current_value(self):
         return self.current + (self.terminal.move_left * self.cursor_offset)
@@ -24,10 +24,21 @@ class Text(BaseConsoleRender):
             raise errors.EndOfInput(self.current)
 
         if pressed == key.TAB and self.question.autocomplete:
-            self.current = str(self.question.autocomplete(self.current, self._autocomplete_state))
-            self.cursor_offset = 0
-            self._autocomplete_state += 1
-        elif pressed == key.BACKSPACE:
+            if self._autocomplete_state is None:
+                self._autocomplete_state = [self.current, 0]
+
+            [text, state] = self._autocomplete_state
+            autocomplete = self.question.autocomplete(text, state)
+            if isinstance(autocomplete, str):
+                self.current = autocomplete
+                self._autocomplete_state[1] += 1
+            else:
+                self._autocomplete_state = None
+            return
+
+        self._autocomplete_state = None
+
+        if pressed == key.BACKSPACE:
             if self.current and self.cursor_offset != len(self.current):
                 if self.cursor_offset > 0:
                     n = -self.cursor_offset
