8e3bdd94b76488945ccd2a913023a97362d612ce
William Venner <14863743+WilliamVenner@users.noreply.github.com>
2022-11-30 10:33:00 +0000

    Improve text autocomplete example

--- examples/text_autocomplete.py
+++ examples/text_autocomplete.py
@@ -6,15 +6,20 @@ from pprint import pprint
 sys.path.append(os.path.realpath("."))
 import inquirer  # noqa
 
+suggestions = ["inquirer", "hello", "world", "foo", "bar", "baz", "qux"]
 
-def autocomplete_fn(_text, _state):
-    return "inquirer"
+
+def autocomplete_fn(_text, state):
+    # Every time the user presses TAB, we'll switch to the next suggestion
+    # The `state` variable contains the index of the current suggestion
+    # We can wrap it around to the first suggestion if we reach the end
+    return suggestions[state % len(suggestions)]
 
 
 questions = [
     inquirer.Text(
         "name",
-        message="Enter the name of this library (Press TAB to autocomplete)",
+        message="Press TAB to cycle through suggestions",
         autocomplete=autocomplete_fn,
     ),
 ]
