afca59e4b61d60dacf3531d2d1119788eb3a949a
Jan Wille <mail@janwille.de>
2023-11-21 19:04:30 +0100

    in reaction to #482 changed selected color to cyan
    the blue used up to now can be very dark and hard to see in some terminals
    (mainly CMD)

--- src/inquirer/themes.py
+++ src/inquirer/themes.py
@@ -88,14 +88,14 @@ class Default(Theme):
         self.Question.brackets_color = term.normal
         self.Question.default_color = term.normal
         self.Editor.opening_prompt_color = term.bright_black
-        self.Checkbox.selection_color = term.blue
+        self.Checkbox.selection_color = term.cyan
         self.Checkbox.selection_icon = ">"
         self.Checkbox.selected_icon = "[X]"
         self.Checkbox.selected_color = term.yellow + term.bold
         self.Checkbox.unselected_color = term.normal
         self.Checkbox.unselected_icon = "[ ]"
         self.Checkbox.locked_option_color = term.gray50
-        self.List.selection_color = term.blue
+        self.List.selection_color = term.cyan
         self.List.selection_cursor = ">"
         self.List.unselected_color = term.normal
 
