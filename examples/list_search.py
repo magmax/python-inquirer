3e3b1efb1a7835de5efbfb726efaf060bdc09dd6
Victor FALAISE <victor.falaise@outlook.com>
2024-02-02 15:28:33 +0100

    Adding an example for the search parameter

---
+++ examples/list_search.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from pprint import pprint
+
+
+sys.path.append(os.path.realpath("."))
+import inquirer  # noqa
+
+
+questions = [
+    inquirer.List(
+        "size", message="What size do you need?", choices=["Jumbo", "Large", "Standard"], carousel=True, search=True
+    ),
+]
+
+answers = inquirer.prompt(questions)
+
+pprint(answers)
